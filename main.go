99af3ab018bc2012a2034f61b909bd95a8bf605a
xjt2976 <xjt2976@gmail.com>
2022-05-23 17:05:36 +0800

    modify main.go

--- main.go
+++ main.go
@@ -5,7 +5,21 @@ import (
 	"main/leetcode1"
 )
 
+type Sum interface {
+	Add() error
+}
+
+type TestSum struct {
+}
+
+func (t *TestSum) Add() error {
+	return nil
+}
+
 func main() {
+	var s Sum
+	s = &TestSum{}
+	_ = s.Add()
 	leetcode1.TwoSum()
 	fmt.Println("hello world")
 }
